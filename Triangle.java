24357bacb6cd57a8c4f73eb4d3eb0125265091df
Ashu-Gan <ashishgangolliis@gmail.com>
2024-09-15 19:43:30 +0530

    Add files via upload
    initial commit

---
+++ Triangle.java
@@ -0,0 +1,19 @@
+package Javabasics;
+
+public class Triangle {
+
+	public static void main(String[] args) {
+	int max=5;
+
+			for (int i=1; i<=max; i++) 
+			{
+				for (int j=1; j<=i; j++) 
+				{
+					System.out.print("*");
+				}
+		System.out.println();
+	}
+
+	}
+
+}
